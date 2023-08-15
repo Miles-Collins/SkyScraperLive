fc95896f167d88d566e2bc28ea694a39c00ab809
Miles-Collins <miles.collins@ymail.com>
2023-08-15 12:37:23 -0600

    sync

--- TA-Tower/server/models/Ticket.js
+++ TA-Tower/server/models/Ticket.js
@@ -1,26 +1,25 @@
 import mongoose, { Schema } from "mongoose";
 
-
-const schema = mongoose.Schema
+const schema = mongoose.Schema;
 
 export const TicketSchema = new Schema(
   {
-    eventId: {type: Schema.Types.ObjectId, ref:'Event', required: true},
-    accountId: {type: Schema.Types.ObjectId, ref:'Account', required: true}
+    eventId: { type: Schema.Types.ObjectId, ref: "Event", required: true },
+    accountId: { type: Schema.Types.ObjectId, ref: "Account", required: true },
   },
-  {timestamps: true, toJSON: {virtuals: true}}
-)
+  { timestamps: true, toJSON: { virtuals: true } }
+);
 
-TicketSchema.virtual('profile', {
-  localField: 'accountId',
-  foreignField: '_id',
-  ref: 'Account',
-  justOne: true
-})
+TicketSchema.virtual("profile", {
+  localField: "accountId",
+  foreignField: "_id",
+  ref: "Account",
+  justOne: true,
+});
 
-TicketSchema.virtual('event', {
-  localField: 'eventId',
-  foreignField: '_id',
-  ref: 'Event',
-  justOne: true
-})
+TicketSchema.virtual("event", {
+  localField: "eventId",
+  foreignField: "_id",
+  ref: "Event",
+  justOne: true,
+});
